69ea44785037f20349bdc52c57d9cb70fbaf0449
prvictorsilvaribeiro-maker <prvictorsilvaribeiro@gmail.com>
2026-09-02 15:57:51 -0300

    Update page.tsx

--- src/app/(app)/enxoval/page.tsx
+++ src/app/(app)/enxoval/page.tsx
@@ -1,11 +1,15 @@
 import { ListaProdutos } from "@/components/ListaProdutos";
 import { brl } from "@/lib/format";
-import { buscarLojas, buscarProdutos, resumir } from "@/lib/queries";
+import { buscarComprasPorProduto, buscarLojas, buscarProdutos, resumir } from "@/lib/queries";
 
 export const dynamic = "force-dynamic";
 
 export default async function EnxovalPage() {
-  const [produtos, lojas] = await Promise.all([buscarProdutos("ENXOVAL"), buscarLojas()]);
+  const [produtos, lojas, compras] = await Promise.all([
+    buscarProdutos("ENXOVAL"),
+    buscarLojas(),
+    buscarComprasPorProduto(),
+  ]);
   const r = resumir(produtos);
 
   return (
@@ -16,7 +20,7 @@ export default async function EnxovalPage() {
         </p>
         <h1 className="font-display text-2xl font-semibold tracking-tight">Enxoval</h1>
       </header>
-      <ListaProdutos produtos={produtos} categoria="ENXOVAL" lojas={lojas} />
+      <ListaProdutos produtos={produtos} categoria="ENXOVAL" lojas={lojas} compras={compras} />
     </>
   );
 }
